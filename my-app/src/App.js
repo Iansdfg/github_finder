3ed3f1e33dd0fa14e393900873613665ded89665
fengtxy <feng.txy@outlook.com>
2020-03-17 16:36:53 -0700

    first

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import React, { Fragment, Component} from 'react';
+import './App.css';
+
+class App extends Component {
+  render(){
+    const name = 'John Doe';
+    const loading = false;
+    const showName = false;
+    return (
+      <div className="App">
+        {loading ? <h4>Loading...</h4>:<h1>Hello {showName && name}</h1>}
+      </div>
+    );
+  }
+}
+
+export default App;
